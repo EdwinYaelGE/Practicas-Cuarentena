be742d89eb1ff651b6a1c13a8a0eb34012933908
EdwinYaelGE <60263398+EdwinYaelGE@users.noreply.github.com>
2020-04-30 18:36:38 -0500

    Add files via upload

---
+++ Calculadora/main.py
@@ -0,0 +1,7 @@
+import Calculadora
+a=int(input("Digite su numero: "))
+b=int(input("Digite su numero: "))
+Calculadora.Suma(a,b);
+Calculadora.Resta(a,b);
+Calculadora.Producto(a,b);
+Calculadora.Division(a,b);
